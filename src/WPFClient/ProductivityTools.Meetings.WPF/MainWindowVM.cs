769f3f53176b19579411c1ac6d83a9ef3d3c5760
pwujczyk <pwujczyk@ecovadis.com>
2019-09-26 03:58:36 +0200

    initial, loading meeting list and new meeting

---
+++ src/WPFClient/ProductivityTools.Meetings.WPF/MainWindowVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ProductivityTools.Meetings.WPF
+{
+    class MainWindowVM
+    {
+        IView View;
+        private DelegateCommand selectMeetingList;
+
+        public MainWindowVM(IView view)
+        {
+            View = view;
+        }
+
+        public DelegateCommand SelectMeetingList
+        {
+            get { return this.selectMeetingList; }
+        }
+
+       
+    }
+}
