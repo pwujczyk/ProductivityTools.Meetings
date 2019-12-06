4df4f84db867d5b275667f398c1067fd86952d7e
pwujczyk <pwujczyk@ecovadis.com>
2019-12-06 16:16:57 +0100

    returning simple list of meetings

---
+++ src/Common/ProductivityTools.Meetings.CoreObjects/Meeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProductivityTools.Meetings.CoreObjects
+{
+    public class Meeting
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string BeforeNotes { get; set; }
+        public string Notes { get; set; }
+        public string AfterNotes { get; set; }
+    }
+}
