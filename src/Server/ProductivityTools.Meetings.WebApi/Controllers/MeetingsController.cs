4c12bdb9176052bf49288a138ac97a132ca34f98
Pawel Wujczyk <pwujczyk@hotmail.com>
2019-12-06 16:07:28 +0100

    method example

---
+++ src/Server/ProductivityTools.Meetings.WebApi/Controllers/MeetingsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductivityTools.Meetings.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MeetingsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Date")]
+        public string GetDate()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
