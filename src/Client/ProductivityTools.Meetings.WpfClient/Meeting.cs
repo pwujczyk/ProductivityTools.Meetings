ddcdebc84ea7baa40a864fa2b30fb71d0f77ab9d
Pawel Wujczyk <pwujczyk@hotmail.com>
2019-12-27 22:52:11 +0100

    binding to button

--- src/Client/ProductivityTools.Meetings.WpfClient/Meeting.cs
+++ src/Client/ProductivityTools.Meetings.WpfClient/Meeting.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace ProductivityTools.Meetings.WpfClient
 {
     public class Meeting
     {
+
+
         public int MeetingId { get; set; }
         public DateTime Date { get; set; }
         public string Subject { get; set; }
         public string BeforeNotes { get; set; }
         public string DuringNotes { get; set; }
         public string AfterNotes { get; set; }
+
+        public ICommand EditMeetingCommand { get; }
+
+        public Meeting()
+        {
+            EditMeetingCommand = new CommandHandler(EditMeetingClick, () => true);
+        }
+
+        private void EditMeetingClick()
+        {
+
+        }
     }
 }
