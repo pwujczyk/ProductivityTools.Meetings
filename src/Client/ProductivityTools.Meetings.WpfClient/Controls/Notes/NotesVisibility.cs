8d210e945ed29cc8f82af9b5a22215c6f5a44377
Pawel Wujczyk <pwujczyk@hotmail.com>
2019-12-29 23:01:11 +0100

    test

---
+++ src/Client/ProductivityTools.Meetings.WpfClient/Controls/Notes/NotesVisibility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.Meetings.WpfClient.Controls.Notes
+{
+    //public enum NotesVisibility:byte
+    //{
+    //    AlwaysVisible=10,
+    //    CollapsedWhenEmpty=20
+    //}
+}
