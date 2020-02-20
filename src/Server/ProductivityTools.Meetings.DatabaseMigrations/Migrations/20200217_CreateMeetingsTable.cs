322ad2c3bac38f07968bd65eff43b013764802b3
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-02-20 23:08:22 +0100

    Automatic Commit

---
+++ src/Server/ProductivityTools.Meetings.DatabaseMigrations/Migrations/20200217_CreateMeetingsTable.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.Meetings.DatabaseMigrations.Migrations
+{
+    [Migration(20200217)]
+    class CreateMeetingsTable : Migration
+    {
+        public override void Down()
+        {
+            Create.Table("Meetings")
+                .WithColumn("MeetingId").AsInt32().Identity().PrimaryKey()
+                .WithColumn("Date").AsDateTime().NotNullable()
+                .WithColumn("Subject").AsString(200).NotNullable()
+                .WithColumn("BeforeNotes").AsString(2000);
+        }
+
+        public override void Up()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
