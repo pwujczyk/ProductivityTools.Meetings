b5feaf0af4c8fa40a0b75972ceee33736e3cbb2a
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-02-20 23:08:22 +0100

    Automatic Commit

---
+++ src/Server/ProductivityTools.Meetings.DatabaseMigrations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductivityTools.Meetings.DatabaseMigrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
