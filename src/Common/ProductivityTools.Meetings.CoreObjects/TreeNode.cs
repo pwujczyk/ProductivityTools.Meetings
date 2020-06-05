a07b17c07e44b8b2621f44e3d98308af53c565f1
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-06-05 19:21:48 +0200

    tree

---
+++ src/Common/ProductivityTools.Meetings.CoreObjects/TreeNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.Meetings.CoreObjects
+{
+    public class TreeNode
+    {
+        public string Name { get; set; }
+        public List<TreeNode> Nodes { get; set; }
+
+        public TreeNode(string name)
+        {
+            this.Name = name;
+            this.Nodes = new List<TreeNode>();
+        }
+    }
+}
---
+++ src/Server/ProductivityTools.Meetings.WebApi/Controllers/TreeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductivityTools.Meetings.CoreObjects;
+
+namespace ProductivityTools.Meetings.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TreeController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Get")]
+        public List<TreeNode> GetTree()
+        {
+            List<TreeNode> result = new List<TreeNode>();
+            result.Add(new TreeNode("EcoVadis"));
+            result.Add(new TreeNode("Pawel"));
+            return result;
+        }
+    }
+}
