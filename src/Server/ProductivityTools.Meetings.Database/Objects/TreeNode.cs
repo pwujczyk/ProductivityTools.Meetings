68162940e04c4966bf1855bd0e1d63bc345a4530
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-06-29 23:55:26 +0200

    database layer

---
+++ src/Server/ProductivityTools.Meetings.Database/Objects/TreeNode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.Meetings.Database.Objects
+{
+    public class TreeNode
+    {
+        public string Name { get; set; }
+    }
+}
--- src/Server/ProducvitityTools.Meetings.Queries/Tree/TreeQueries.cs
+++ src/Server/ProducvitityTools.Meetings.Queries/Tree/TreeQueries.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProductivityTools.Meetings.Database;
+using ProductivityTools.Meetings.Database.Objects;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,19 @@ namespace ProducvitityTools.Meetings.Queries
 {
     class TreeQueries
     {
+        MeetingContext MeetingContext;
+
+        public TreeQueries(MeetingContext context)
+        {
+            this.MeetingContext = context;
+        }
+
+        public List<TreeNode> GetTree()
+        {
+            var result = this.MeetingContext..OrderByDescending(x => x.Date).ToList();
+            return result;
+        }
+
         public void GetTree()
         {
 
