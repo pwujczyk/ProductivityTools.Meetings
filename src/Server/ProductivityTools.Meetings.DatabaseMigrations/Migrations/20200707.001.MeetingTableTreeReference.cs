464cc01854f6391d2dbb5aa574366068d49fdb99
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-07-07 14:54:38 +0200

    meeting reference

---
+++ src/Server/ProductivityTools.Meetings.DatabaseMigrations/Migrations/20200707.001.MeetingTableTreeReference.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.Meetings.DatabaseMigrations.Migrations
+{
+    [Migration(20200707001)]
+    public class MeetingTableTreeReferenc : Migration
+    {
+        public override void Down()
+        {
+            
+        }
+
+        public override void Up()
+        {
+            Alter.Table("Meeting")
+                .InSchema("mt").AddColumn("TreeId").AsInt32().Nullable();
+            Create.ForeignKey().FromTable("Meeting").InSchema("mt").ForeignColumn("TreeId")
+                .ToTable("Tree").InSchema("mt").PrimaryColumn("TreeId");
+        }
+    }
+}
