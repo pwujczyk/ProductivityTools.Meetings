82e65699b23fb23332f212f845bcb8760881a32a
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-08-02 21:13:21 +0200

    echo controller

---
+++ src/Server/ProductivityTools.Meetings.WebApi/Controllers/EchoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductivityTools.Meetings.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EchoController : Controller
+    {
+        [HttpGet]
+        [Route("Date")]
+        public string Date()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        [HttpPost]
+        [Route("Hello")]
+        public string Hello(object s)
+        {
+            return string.Concat($"Hello {s.ToString()} Current date:{DateTime.Now}");
+        }
+    }
+}
